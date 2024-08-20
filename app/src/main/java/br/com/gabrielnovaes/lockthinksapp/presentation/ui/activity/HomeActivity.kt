c42cdc942a8f32507955837282949d50ebddf1d0
Gabriel Novaes <gabrielnovaes@yahoo.com.br>
2024-08-20 20:12:51 -0300

    build: add dependecies to architecture and tests

--- app/src/main/java/br/com/gabrielnovaes/lockthinksapp/presentation/ui/activity/HomeActivity.kt
+++ app/src/main/java/br/com/gabrielnovaes/lockthinksapp/presentation/ui/activity/HomeActivity.kt
@@ -2,11 +2,22 @@ package br.com.gabrielnovaes.lockthinksapp.presentation.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import br.com.gabrielnovaes.lockthinksapp.R
+import br.com.gabrielnovaes.lockthinksapp.databinding.ActivityMainBinding
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class HomeActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+         val fab: FloatingActionButton = findViewById(R.id.fab)
+
+         fab.setOnClickListener {
+            Toast.makeText(this, "Fab Button Clicked!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
