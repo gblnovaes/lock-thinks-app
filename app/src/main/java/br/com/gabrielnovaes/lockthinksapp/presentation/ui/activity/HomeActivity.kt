70c58fc0b3d0c7fbd792b9d2d140a4a6789f2d96
Gabriel Novaes <gabrielnovaes@yahoo.com.br>
2024-08-20 22:25:52 -0300

    chore: added layout to show lock status and dialog to register tag

--- app/src/main/java/br/com/gabrielnovaes/lockthinksapp/presentation/ui/activity/HomeActivity.kt
+++ app/src/main/java/br/com/gabrielnovaes/lockthinksapp/presentation/ui/activity/HomeActivity.kt
@@ -2,22 +2,63 @@ package br.com.gabrielnovaes.lockthinksapp.presentation.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import br.com.gabrielnovaes.lockthinksapp.R
-import br.com.gabrielnovaes.lockthinksapp.databinding.ActivityMainBinding
-import com.google.android.material.floatingactionbutton.FloatingActionButton
+import br.com.gabrielnovaes.lockthinksapp.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
 
-     override fun onCreate(savedInstanceState: Bundle?) {
+    private lateinit var binding: ActivityHomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.fab.setOnClickListener {
+            Toast.makeText(
+                this,
+                "Fab Button Clicked!",
+                Toast.LENGTH_SHORT
+            ).show()
 
-
-         val fab: FloatingActionButton = findViewById(R.id.fab)
-
-         fab.setOnClickListener {
-            Toast.makeText(this, "Fab Button Clicked!", Toast.LENGTH_SHORT).show()
+            showTagRegisterDialog()
         }
+
+        binding.lockTagBtnOpen.setOnClickListener {
+            Toast.makeText(this, "Button Open Clicked!", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
+    private fun showTagRegisterDialog() {
+
+        val inflater = LayoutInflater.from(this)
+        val dialogView = inflater.inflate(R.layout.custom_dialog_register_tag, null)
+
+        val dialogBuilder = AlertDialog.Builder(this)
+            .setView(dialogView)
+            .setCancelable(true)
+
+        val alertDialog = dialogBuilder.create()
+
+        val inputField = dialogView.findViewById<EditText>(R.id.dialog_input)
+        val cancelButton = dialogView.findViewById<Button>(R.id.button_cancel)
+        val registerButton = dialogView.findViewById<Button>(R.id.button_register)
+
+        cancelButton.setOnClickListener {
+            alertDialog.dismiss()
+        }
+
+        registerButton.setOnClickListener {
+            val userInput = inputField.text.toString()
+            println("Registro: $userInput")
+            alertDialog.dismiss()
+        }
+
+        alertDialog.show()
     }
 }
